391f537248fb7a720df02985004dd0c138858451
Dmitry <example@gmail.ru>
2019-09-09 12:54:06 +0300

    task thirteen

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -96,11 +96,62 @@ window.addEventListener('DOMContentLoaded', () => {
     overlay.style.display = 'block';
     more.classList.add('more-splash');
     document.body.style.overflow = 'hidden';
-  })
+  });
 
   close.addEventListener('click', () => {
     overlay.style.display = 'none';
     more.classList.remove('more-splash');
     document.body.style.overflow = '';
-  })
+  });
+
+  //Form
+
+  let message = {
+    loading: 'Loading...',
+    success: 'Thanks! We will contact you soon!',
+    failure: 'Something went wrong...'
+  };
+
+  let form = document.querySelector('.main-form'),
+      input = form.getElementsByTagName('input'),
+      statusMessage = document.createElement('div');
+
+      statusMessage.classList.add('status');
+
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+
+        request.open('POST', 'server.php');
+        //request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach((value, key) => {
+          obj[key] = value;
+        });
+
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', () => {
+          if ( request.readyState < 4) {
+            statusMessage.innerHTML = message.loading;
+          } else if (request.readyState === 4 && request.status === 200) {
+            statusMessage.innerHTML = message.success;
+          } else {
+            statusMessage.innerHTML = message.failure;
+          }
+        });
+
+          for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+          }
+
+      })
 });
