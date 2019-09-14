58ce4c1bb062f69441a932a4b66c2293b0513021
Dmitry <example@gmail.ru>
2019-09-14 15:29:03 +0300

    create calc

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -224,4 +224,47 @@ window.addEventListener('DOMContentLoaded', () => {
 
   setInterval(autoSlider, 5000);
 
+  // Calc
+
+  let persons = document.querySelectorAll('.counter-block-input')[0],
+      restDays = document.querySelectorAll('.counter-block-input')[1],
+      place = document.getElementById('select'),
+      totalValue = document.getElementById('total'),
+      personsSum = 0,
+      daysSum = 0,
+      total = 0;
+
+      totalValue.textContent = 0;
+
+      persons.addEventListener('change', function() {
+        personsSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if (restDays.value == '') {
+          totalValue.textContent = 0;
+        } else {
+          totalValue.textContent = total;
+        }
+      });
+
+      restDays.addEventListener('change', function() {
+        daysSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if (persons.value == '') {
+          totalValue.textContent = 0;
+        } else {
+          totalValue.textContent = total;
+        }
+      });
+
+      place.addEventListener('change', function() {
+        if (restDays.value == '' || persons.value == '') {
+          totalValue.textContent = 0;
+        } else {
+          let a = total;
+          totalValue.textContent = a * this.options[this.selectedIndex].value;
+        }
+      })
+
 });
